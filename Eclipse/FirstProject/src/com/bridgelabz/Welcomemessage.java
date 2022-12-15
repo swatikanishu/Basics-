de298ec168663c102eafd1ddf98b2a2abd25f5ee
swatikanishu <nishuswatika@gmail.com>
2022-12-15 11:49:01 +0530

    added

---
+++ Eclipse/FirstProject/src/com/bridgelabz/Welcomemessage.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+public class Welcomemessage {
+
+	public static void main(String[] args) {
+		int a=9;
+		if(a==10) {
+			System.out.println("hiiiiiiiiiiiiii");	
+		}else {
+			System.out.println("hello");
+		}
+		
+	}
+
+
+}
---
+++ Eclipse/SecondProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author lenovo
+ *
+ */
+module SecondProject {
+}
